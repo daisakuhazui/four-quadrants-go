17099a2c58f6108bfe2da2eb1039798b1dbb4603
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2019-10-13 14:07:18 +0900

    Add api functions about Task

--- backend/tasks.go
+++ backend/tasks.go
@@ -3,10 +3,21 @@ package backend
 import (
 	"net/http"
 
-	"github.com/daisakuhazui/four-quadrants-go/common"
 	"github.com/labstack/echo"
 )
 
 func handlerTaskGet(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{"hello": "world"})
+}
+
+func handlerTaskPost(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"task": "post"})
+}
+
+func handlerTaskPut(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"task": "put"})
+}
+
+func handlerTaskDelete(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"task": "delete"})
 }
