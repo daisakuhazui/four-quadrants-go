71f23f9c36b5a3480d0b7de93555a16e50506a26
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2019-10-19 11:32:38 +0900

    Add routing about tasks-index and tasks

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TasksIndexComponent } from './tasks-index/tasks-index.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes: Routes = [
+  {path: '', component: TasksIndexComponent},
+  {path: 'tasks', component: TasksComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
