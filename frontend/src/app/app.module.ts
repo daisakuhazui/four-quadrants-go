5a2804ec9b735aec5e4a1e6a63dbb648ecc63653
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2019-10-19 11:06:38 +0900

    Genarate tasks-index component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksIndexComponent } from './tasks-index/tasks-index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksIndexComponent
   ],
   imports: [
     BrowserModule,
