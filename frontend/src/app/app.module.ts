a5537e6500a2b1eb71f6b89fec29a8c0a9d1d7e7
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2019-11-26 07:30:18 +0900

    Generate user-attendance component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,12 +9,20 @@ import { HttpClientService } from './service/http-client.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserAttendanceComponent } from './components/user-attendance/user-attendance.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksIndexComponent,
-    TasksComponent
+    TasksComponent,
+    UserAttendanceComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +30,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     HttpClientService
---
+++ frontend/src/app/components/user-attendance/user-attendance.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-user-attendance',
+  templateUrl: './user-attendance.component.html',
+  styleUrls: ['./user-attendance.component.css']
+})
+export class UserAttendanceComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
