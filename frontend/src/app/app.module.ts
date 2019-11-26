c634c3b85f8cdba8037a89e4f59f59e967a24962
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2019-11-27 06:51:18 +0900

    Generate components/navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,13 +16,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksIndexComponent,
     TasksComponent,
-    UserAttendanceComponent
+    UserAttendanceComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +40,9 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    FlexLayoutModule,
+    MatMenuModule
   ],
   providers: [
     HttpClientService
